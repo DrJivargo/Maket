811671f3764ad58a77ed7e35f843fcd9aa375706
Салават <salavat.kashapov.99@mail.ru>
2025-05-05 18:26:21 +0300

    jokehub repository

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import {AppComponent} from './app.component';
+import {InfoPageComponent} from './pages/info-page/info-page.component';
+import {LoginPageComponent} from './pages/login-page/login-page.component';
+import {OrderComponent} from './pages/order/order.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component:  AppComponent},
+  { path: 'info', component:  InfoPageComponent},
+  { path: 'login', component: LoginPageComponent},
+  { path: 'order', component:  OrderComponent},
+];
